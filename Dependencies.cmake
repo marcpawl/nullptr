321634981ade7b114380e0e3734eb96d6a2885ee
Marc Pawlowsky <marcpawl@gmail.com>
2025-04-16 20:50:41 -0400

    Remove unused dependencies: spdlog and CLI
    CLI was causing problems with the github CI,
    probably just needs a version tweak to work with
    a newer cmake.

--- Dependencies.cmake
+++ Dependencies.cmake
@@ -12,24 +12,9 @@ function(nullptr_setup_dependencies)
     cpmaddpackage("gh:fmtlib/fmt#9.1.0")
   endif()
 
-  if(NOT TARGET spdlog::spdlog)
-    cpmaddpackage(
-      NAME
-      spdlog
-      VERSION
-      1.11.0
-      GITHUB_REPOSITORY
-      "gabime/spdlog"
-      OPTIONS
-      "SPDLOG_FMT_EXTERNAL ON")
-  endif()
 
   if(NOT TARGET Catch2::Catch2WithMain)
     cpmaddpackage("gh:catchorg/Catch2@3.3.2")
-  endif()
-
-  if(NOT TARGET CLI11::CLI11)
-    cpmaddpackage("gh:CLIUtils/CLI11@2.3.2")
   endif()
 
   if(NOT TARGET tools::tools)
